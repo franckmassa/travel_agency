46b82c089c023b3c02804ca5487134d76c2fd57c
massa <massafranck@gmail.com>
2019-01-14 15:33:54 +0100

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr "dir="ltr">
+    <head>
+        <title>Site voyage</title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+        <p>Hello!</p>
+
+
+    </body>
+
+</html>
+
